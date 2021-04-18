55405ecfb80d8a9555242d38128f14f133590836
Arnav Kumar <72879799+arnav-kr@users.noreply.github.com>
2021-04-18 13:49:57 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,17 @@
 self.skipWaiting();
+self.addEventListener('push', function(ev) {
+  console.log('[Service Worker] Received a push message\n');
+  let payload = ev.data.json()
+  console.log(payload)
+  if (payload.notification && payload.notification.title) {
+    self.registration.showNotification(payload.notification.title, {
+      body: payload.notification.body,
+      icon: 'arnav.png',
+      badge: 'arnav.png',
+      image: payload.notification.image,
+      data: payload.data
+    })
+  } else {
+    console.log('[Service Worker] No notification payload, not showing notification')
+  }
+});
